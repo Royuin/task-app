cde65f6515d737c6afed5d8bc35cfb6fafd3f8d5
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-24 22:07:59 -0500

    made form Component and function to handle submit

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -5,11 +5,28 @@ class Overview extends Component {
     super(props);
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <>
         <h1>Task App</h1>
+        <Form />
       </>
+    );
+  }
+}
+
+class Form extends Overview {
+  render() {
+    return (
+      <form>
+        <label for="task">New Task </label>
+        <input type="text" id="task" name="task" required></input>
+        <button onClick={this.handleSubmit}>Add task</button>
+      </form>
     );
   }
 }
