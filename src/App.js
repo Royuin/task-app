215f297c9db8cbb78b42858c7034feb6600c142e
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-24 21:05:30 -0500

    set up import and exports for App and Overview and made Overview display h1 for title of page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Overview from './components/Overview';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Overview />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Task App</h1>
+      </>
+    );
+  }
+}
+
+export default Overview;
