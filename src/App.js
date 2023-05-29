95c04a4eec431776569983b420d343a40c5b0ad1
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-28 21:22:00 -0500

    added handleSubmit function no functionality added besides peventDefault

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Overview from './components/Overview';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
