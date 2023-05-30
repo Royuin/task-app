ab45a65470b588cef510147c3056012fc00aa724
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-29 19:41:13 -0500

    made App component have tasks array and handleSubmit add value of input to array

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.tasks = [];
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    const thisTask = document.getElementById('task').value;
+    this.tasks.push(thisTask);
   }
 
   render() {
     return (
       <div className="App">
-        <Overview handleSubmit={this.handleSubmit} />
+        <Overview handleSubmit={this.handleSubmit} tasks={this.tasks} />
       </div>
     );
   }
