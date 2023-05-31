91f130d49b0b6cf11094f8c09cd8e66f8029af53
Gage <77414756+Royuin@users.noreply.github.com>
2023-05-30 19:41:54 -0500

    fixed sending state.tasks prop array being sent to Overview.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Overview handleSubmit={this.handleSubmit} tasks={this.tasks} />
+        <Overview handleSubmit={this.handleSubmit} state={this.state} />
       </div>
     );
   }
